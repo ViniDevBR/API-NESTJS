c1f06268ebcb55cfc2c71e99e54705f6c3c38d73
ViniDevBR <vinigtr386@gmail.com>
2025-05-16 10:01:07 -0300

    :fire: feat: config do banco de dados

---
+++ transacao/prisma/migrations/20250515143427_create_transactions_table/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Transactions" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "amount" INTEGER NOT NULL,
+    "timestamp" DATETIME NOT NULL
+);
---
+++ transacao/src/constants/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable prettier/prettier */
+export const ONE_MINUTE_ML = 60000;
