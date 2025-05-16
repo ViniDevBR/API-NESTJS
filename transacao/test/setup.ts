90266aa6eafa164dd17e4927c0f70a9cd662ffe3
ViniDevBR <vinigtr386@gmail.com>
2025-05-16 12:27:30 -0300

    :fire: chore: git actions setup env

--- transacao/test/setup.ts
+++ transacao/test/setup.ts
@@ -1,4 +1,4 @@
 /* eslint-disable prettier/prettier */
-import dotenv from 'dotenv';
-dotenv.config(); // Vai carregar o .env dentro de transacao, pois o cwd é transacao
-console.log('DATABASE_URL:', process.env.DATABASE_URL)
+// import dotenv from 'dotenv';
+// dotenv.config(); // Vai carregar o .env dentro de transacao, pois o cwd é transacao
+// console.log('DATABASE_URL:', process.env.DATABASE_URL)
